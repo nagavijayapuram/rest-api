6e5985b71b6c93d99ca9ecde48340ab529d34a0c
Naga Vijayapuram <ctciglm@gmail.com>
2021-11-06 19:41:51 -0700

    enhanced main.sh

--- go/main.sh
+++ go/main.sh
@@ -4,7 +4,18 @@
 # main.sh
 # --------
 
-go run main.go 2>/dev/null &
+cd `dirname $0`
+
+trap "/bin/rm -f main" EXIT SIGINT SIGTERM
+
+nc -zv localhost 8000 2>/dev/null
+
+if [ $? -eq 0 ]; then
+  echo -e "\n . main is already up and running; halt it first and then rerun this script\n"
+  exit 1
+fi
+
+go run main.go &
 
 sleep 1
 
